deed33cba33a017c79456cfefa20f207496656f2
Piyush Bharti <piyushbharti338@gmail.com>
2026-07-23 10:25:37 +0530

    Update frontend API URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Set CORS for frontend URL / allow single-node deploy
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
   credentials: true
 }));
 
